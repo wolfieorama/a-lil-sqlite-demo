1cded2d6ac20cf899a8912c0baf5fc57989fe008
John Munyi <wachirawamunyi@gmail.com>
2017-11-18 15:05:45 +0200

    added classes Students and Quiz

---
+++ app/src/main/java/com/example/johnmunyi/studentrecords/Quizes.java
@@ -0,0 +1,58 @@
+package com.example.johnmunyi.studentrecords;
+
+/**
+ * Created by johnmunyi on 11/18/17.
+ */
+
+public class Quizes {
+    private String quiz_num;
+    private int id,courseId, studentId, score;
+
+    public Quizes(String quiz_num, int id, int courseId, int studentId, int score) {
+        this.quiz_num = quiz_num;
+        this.id = id;
+        this.courseId = courseId;
+        this.studentId = studentId;
+        this.score = score;
+    }
+
+    public String getQuiz_num() {
+        return quiz_num;
+    }
+
+    public void setQuiz_num(String quiz_num) {
+        this.quiz_num = quiz_num;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
---
+++ app/src/main/java/com/example/johnmunyi/studentrecords/Students.java
@@ -0,0 +1,37 @@
+package com.example.johnmunyi.studentrecords;
+
+/**
+ * Created by johnmunyi on 11/18/17.
+ */
+
+public class Students {
+
+    private int id;
+    private String name;
+
+    public Students(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return ":" + name;
+    }
+}
